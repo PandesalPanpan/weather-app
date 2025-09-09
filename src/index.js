fca91efa56875e0658756016edb6f74147341bb3
PandesalPanpan <petermarticio@gmail.com>
2025-09-09 08:56:49 +0800

    Feat: form event listener and triggering weather request and requesting gif based on the conditions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./styles.css";
+import WeatherAPI from "./weather";
+import GiphyAPI from "./giphy";
 
+const weatherGif = document.querySelector('#weather-gif')
+const form = document.querySelector('form');
+const conditionDOM = document.querySelector("#weather-condition");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const query = formData.get("search");
+    WeatherAPI.getWeather(query)
+    .then(response => {
+        return response.currentConditions.conditions;
+    })
+    .then(conditions => {
+        conditionDOM.textContent = `${conditions}`;
+        return GiphyAPI.getGIF(conditions);
+    })
+    .then(src => {
+        weatherGif.src = src;
+    })
+
+})
